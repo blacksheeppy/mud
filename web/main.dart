d82187682fe5f8f004587627a41728df84042e0b
Jose Segovia <jose.segovia@gmail.com>
2016-11-21 01:40:18 -0300

    First commit

---
+++ web/main.dart
@@ -0,0 +1,70 @@
+// Copyright (c) 2016, Jose Segovia. All rights reserved. Use of this source code
+
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'dart:html';
+
+void main() {
+  querySelector("#enter").onClick.listen(clickedOnEnter);
+}
+
+Environment myEnvironment = new Environment("wood");
+
+clickedOnEnter(e) {
+  InputElement input = querySelector("#commands");
+  input.style.borderColor = "#55ff55";
+  String output = "";
+  switch (input.value) {
+    case 'left':
+      output = "You are going left now";
+      break;
+    case 'right':
+      output = "You are going right now";
+      break;
+    case 'up':
+      output = "You are going up now";
+      break;
+    case 'down':
+      output = "You are going down now";
+      break;
+    default:
+      output = "Use left, rigth, up, down please ...";
+  }
+  var outputHTML = "<div>${output}</div>";
+  querySelector("#output").appendHtml(outputHTML);
+  var interaction = myEnvironment.stumbleUpon();
+  outputHTML = "<div>${interaction}</div>";
+  querySelector("#output").appendHtml(outputHTML);
+}
+
+class Environment {
+
+  String name;
+  Encounter encounter = new Bear();
+
+  Environment(this.name);
+
+  String stumbleUpon() {
+    return "${this.name} You stumble upon something ${this.encounter.whenEncounter()}";
+  }
+}
+
+abstract class Encounter {
+  whenEncounter();
+}
+
+class Bear extends Encounter {
+
+  @override
+  whenEncounter() {
+    return "grr grow, You encounter a bear!";
+  }
+}
+
+class Ghost extends Encounter {
+
+  @override
+  whenEncounter() {
+    return "Booh, you encounter a Ghost!";
+  }
+}
